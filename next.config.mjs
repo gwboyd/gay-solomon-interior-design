2ccda3a7cd74daeda3fb1111966550f0eff05c76
gwboyd <willboyd970@gmail.com>
2026-01-03 22:39:54 -0500

    package bump

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-import withPWA from 'next-pwa'
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   eslint: {
@@ -11,13 +9,6 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-}
+};
 
-const config = withPWA({
-  dest: 'public',
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === 'development'
-})(nextConfig)
-
-export default config
+export default nextConfig;
